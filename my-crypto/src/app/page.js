b353276d9427458e2ccfb38311ddefc04b5c389d
ragu47 <ragu.p2100@gmail.com>
2024-03-09 19:43:27 +0530

    new changes

--- my-crypto/src/app/page.js
+++ my-crypto/src/app/page.js
@@ -3,10 +3,10 @@
 import axios from "axios";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import {useForm} from 'react-hook-form'
-import * as yup from 'yup'
-import {yupResolver} from '@hookform/resolvers/yup'
-
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { ScaleLoader } from "react-spinners";
 
 // Main component
 export default function Home() {
@@ -24,7 +24,14 @@ export default function Home() {
   const [dropdown2, setDropdown2] = useState(false);
   const [selectedCryptoName1, setSelectedCryptoName1] = useState("");
   const [selectedCryptoName2, setSelectedCryptoName2] = useState("");
-  
+
+  const validationSchema = yup.object().shape({
+    amount: yup.string().required('field is required')
+  });
+
+  const { register, handleSubmit,formState:{errors,isValid,isDirty} } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
 
   // Fetch cryptocurrency list on component mount
   useEffect(() => {
@@ -56,17 +63,20 @@ export default function Home() {
       crypto.name.toLowerCase().includes(cryptoSearchQuery.toLowerCase()) ||
       crypto.symbol.toLowerCase().includes(cryptoSearchQuery.toLowerCase())
   );
-
-  // Handle currency swap
-  const handleSwap = async () => {
-    setLoading(true);
+  console.log('err',errors)
   
+  const handleSwap = async (formData) => {
+    const { amount } = formData;
+ 
+
+    setLoading(true);
+
     try {
-      // Mock API call to get exchange rate
+      // Your API call using the extracted data...
       const response = await axios.get(
         `https://api.coingecko.com/api/v3/simple/price?ids=${crypto1},${crypto2}&vs_currencies=usd`
       );
-  
+
       const rate = response.data[crypto2].usd;
       setEstimatedAmount(amount * rate);
       setError(""); // Reset the error state on success
@@ -76,7 +86,6 @@ export default function Home() {
       setLoading(false);
     }
   };
-  
 
   // Handle search input change
   const handleSearchChange = (e) => {
@@ -128,14 +137,17 @@ export default function Home() {
               <span className="text-gray-500 sm:text-sm">$</span>
             </div>
             <input
+              {...register("amount")}
               type="text"
-              name="price"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              id="price"
-              className="bg-customColor1 border border-transparent h-36 block w-full rounded-md py-1.5 pl-7 pr-20 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-transparent sm:text-sm sm:leading-6"
+              name="amount"
+              className={`bg-customColor1 border ${
+                errors?.amount ? "border-red-500" : "border-transparent"
+              } h-36 block w-full rounded-md py-1.5 pl-7 pr-20 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-transparent sm:text-sm sm:leading-6`}
               placeholder="0.00"
             />
+            {errors?.amount && (
+              <p className="text-red-500">{errors.amount.message}</p>
+            )}
             <div className="absolute inset-y-0 right-0 flex items-center">
               {/* Dropdown for the first cryptocurrency */}
               <div className="relative inline-block text-left">
@@ -144,7 +156,7 @@ export default function Home() {
                   onClick={toggleDropdown1}
                   className="inline-flex justify-between items-center w-full px-5 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-800 transition ease-in-out duration-150"
                 >
-                 {selectedCryptoName1 || "Select a cryptocurrency"}
+                  {selectedCryptoName1 || "Select a cryptocurrency"}
                   <svg
                     className="w-5 h-5 ml-2 -mr-1"
                     fill="none"
@@ -164,31 +176,34 @@ export default function Home() {
                 {/* Dropdown panel */}
                 <div
                   className={`absolute left-60 top-1.5 overflow-scroll border-transparent overflow-x-hidden h-56 mt-2 w-200 bg-customColor1 border border-gray-300 rounded-md shadow-md ${
-                    dropdown1 ? "block animate__animated animate__fadeIn" : "hidden"
+                    dropdown1
+                      ? "block animate__animated animate__fadeIn"
+                      : "hidden"
                   }`}
                 >
                   {/* Search input bar */}
                   <div className="flex">
-                  <h1 className="border-transparent">Select a token</h1>
-                  <button
-                    type="button"
-                    onClick={toggleDropdown1}
-                    className="inline-flex justify-between items-center w-20 ml-24 px-5 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-800 transition ease-in-out duration-150 transform hover:scale-105"                  >
-                    <svg
-                      className="w-5 h-5 ml-2 -mr-1 cursor-pointer text-gray-500"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
+                    <h1 className="border-transparent">Select a token</h1>
+                    <button
+                      type="button"
+                      onClick={toggleDropdown1}
+                      className="inline-flex justify-between items-center w-20 ml-24 px-5 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-800 transition ease-in-out duration-150 transform hover:scale-105"
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
+                      <svg
+                        className="w-5 h-5 ml-2 -mr-1 cursor-pointer text-gray-500"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
                   </div>
                   <input
                     type="text"
@@ -242,7 +257,8 @@ export default function Home() {
                 <button
                   type="button"
                   onClick={toggleDropdown2}
-                  className="inline-flex justify-between items-center w-full px-5 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-800 transition ease-in-out duration-150 transform hover:scale-105"                >
+                  className="inline-flex justify-between items-center w-full px-5 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-800 transition ease-in-out duration-150 transform hover:scale-105"
+                >
                   {selectedCryptoName2 || "Select a cryptocurrency"}
                   <svg
                     className="w-5 h-5 ml-2 -mr-1"
@@ -262,32 +278,34 @@ export default function Home() {
 
                 {/* Dropdown panel */}
                 <div
-                 className={`absolute left-60 top-1.5 overflow-scroll border-transparent overflow-x-hidden h-56 mt-2 w-200 bg-customColor1 border border-gray-300 rounded-md shadow-md ${
-                  dropdown2 ? "block animate__animated animate__fadeIn" : "hidden"
-                }`}
+                  className={`absolute left-60 top-1.5 overflow-scroll border-transparent overflow-x-hidden h-56 mt-2 w-200 bg-customColor1 border border-gray-300 rounded-md shadow-md ${
+                    dropdown2
+                      ? "block animate__animated animate__fadeIn"
+                      : "hidden"
+                  }`}
                 >
                   <div className="flex">
-                  <h1 className="border-transparent">Select a token</h1>
-                  <button
-                    type="button"
-                    onClick={toggleDropdown2}
-                    className="inline-flex justify-between items-center w-20 ml-24 px-5 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-800 transition ease-in-out duration-150 transform hover:scale-105"                  >
-                  
-                    <svg
-                      className="w-5 h-5 ml-2 -mr-1 cursor-pointer text-gray-500"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
+                    <h1 className="border-transparent">Select a token</h1>
+                    <button
+                      type="button"
+                      onClick={toggleDropdown2}
+                      className="inline-flex justify-between items-center w-20 ml-24 px-5 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-800 transition ease-in-out duration-150 transform hover:scale-105"
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
+                      <svg
+                        className="w-5 h-5 ml-2 -mr-1 cursor-pointer text-gray-500"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
                   </div>
                   <input
                     type="text"
@@ -317,14 +335,16 @@ export default function Home() {
 
         {/* Swap button */}
         <button
-          onClick={handleSwap}
-          disabled={loading}
+          onClick={handleSubmit(handleSwap)}
+          disabled={isValid && loading &&isDirty}
           className="bg-cyan-700 hover:bg-indigo-600 w-full text-white font-bold my-2 mx-auto py-2 px-4 rounded focus:outline-none shadow-md"
         >
-          Swap
+           {loading ? (
+            <ScaleLoader color="#fff" height={15} width={2} radius={1} margin={2} />
+          ) : (
+            "Swap"
+          )}
         </button>
-
-        {loading && <p className="mt-2 text-gray-700">Loading...</p>}
         {error && <p className="mt-2 text-red-500">{error}</p>}
       </div>
     </main>
